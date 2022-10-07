05cdd608c2e042e8956fd9b601ddb4b8a871c3f1
sh970901 <aaddss639@naver.com>
2022-10-07 10:23:26 +0900

    access토큰 생성 및 static에서 bean활용하기

--- src/main/java/com/ll/exam/app_jwt/AppJwtApplication.java
+++ src/main/java/com/ll/exam/app_jwt/AppJwtApplication.java
@@ -1,7 +1,9 @@
 package com.ll.exam.app_jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppJwtApplication {
@@ -9,5 +11,8 @@ public class AppJwtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppJwtApplication.class, args);
 	}
-
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
---
+++ src/main/java/com/ll/exam/app_jwt/app/AppConfig.java
@@ -0,0 +1,17 @@
+package com.ll.exam.app_jwt.app;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Getter
+    private static ApplicationContext context;
+
+    @Autowired
+    public void setContext(ApplicationContext context) {
+        AppConfig.context = context;
+    }
+}
